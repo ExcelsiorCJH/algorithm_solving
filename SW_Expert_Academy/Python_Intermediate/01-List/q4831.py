e52959b01fbf1c0d5a2c83939b1af5831afe3f65
excelsiorcjh <excelsiorcjh@gmail.com>
2020-06-09 23:16:15 +0900

    전기버스 다시 풀기

--- SW_Expert_Academy/Python_Intermediate/01-List/q4831.py
+++ SW_Expert_Academy/Python_Intermediate/01-List/q4831.py
@@ -2,34 +2,39 @@
 전기 버스
 """
 
-def solution(K, N, charges):
+def solution(K, N, c_list):
     K = int(K)
     N = int(N)
+    c_list = list(map(int, c_list.split()))
+    stations = [0] * (N+1)
+    
+    for idx in range(1, N+1):
+        if idx in c_list:
+            stations[idx] = 1
+            
+    start = 0
+    end = K
+    cnt = 0
+    
+    while True:
+        zero = 0
+        for i in range(start+1, end+1):
+            if stations[i] == 1:
+                start = i
+            else:
+                zero += 1
 
-    stations = list(range(1, N+1))[::-1]
-    l = len(stations)
-    charges = list(map(int, charges.split()))
+        if zero == K:
+            cnt = 0
+            break
 
-    c_idx = 1
-    total_cnt = 0
-    while stations and len(stations) > K:
-        print(stations)
-        cnt = 0
-        curr_charges = [c for c in charges if c <= K*c_idx]
-        print(curr_charges)
-        for _ in range(K):
-            curr = stations.pop()
-            if curr in curr_charges:
-                cnt += 1
-                print(cnt)
-        
-        c_idx += 1
-        if cnt >= 1:
-            total_cnt += 1
-        else:
-            return 0
-
-    return total_cnt
+        cnt += 1
+        end = start + K
+    
+        if end >= N:
+            break
+            
+    return cnt
 
 
 if __name__ == "__main__":
