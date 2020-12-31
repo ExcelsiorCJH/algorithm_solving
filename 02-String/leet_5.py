f5b27aac8685d71c4a0a62559659f288e96e6275
excelsiorcjh <excelsiorcjh@gmail.com>
2020-12-31 22:51:15 +0900

    leet5 - again

--- 02-String/leet_5.py
+++ 02-String/leet_5.py
@@ -20,3 +20,32 @@ class Solution:
 
         return result
 
+
+class Solution2:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2 or s == s[::-1]:
+            return s
+
+        start, r = 0, 1
+        for i in range(1, len(s)):
+            sub1 = s[i - r - 1 : i + 1]
+            sub2 = s[i - r : i + 1]
+
+            if i - r - 1 >= 0 and sub1 == sub1[::-1]:
+                start = i - r - 1
+                r += 2
+            elif i - r >= 0 and sub2 == sub2[::-1]:
+                start = i - r
+                r += 1
+
+        return s[start : start + r]
+
+
+if __name__ == "__main__":
+    s = "babad"
+
+    sol1 = Solution()
+    print(sol1.longestPalindrome(s))
+
+    sol2 = Solution2()
+    print(sol2.longestPalindrome(s))
